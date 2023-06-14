366e007e806d8f5bce8c224b25a6d212f207d819
LVNhaule <lnhaule25@andover.edu>
2023-06-14 15:05:07 -0400

    commit

--- FinalProject/QuotePage.swift
+++ FinalProject/QuotePage.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct QuotePage: View {
+    var quotes = ["Success isn’t always about greatness. It’s about consistency. Consistent hard work leads to success. Greatness will come.\"  \n\n -Dwayne - The Rock - Johnson",
+                  "Dreams can come true, but there is a secret. They’re realized through the magic of persistence, determination, commitment, passion, practice, focus and hard work. They happen a step at a time, manifested over years, not weeks.\" \n\n -Elbert Hubbard",
+                  "Perseverance is the hard work you do after you get tired of doing the hard work you already did.\" \n\n -Newt Gingrich",
+                  "If you go to work on your goals, your goals will go to work on you. If you go to work on your plan, your plan will go to work on you. Whatever good things we build end up building us.\" \n\n -Jim Rohn",
+                  "Work hard in silence; let success make the noise.\" \n\n -Frank Ocean",
+                  "What comes easy won’t last. What lasts won’t come easy.\" \n\n -Unknown",
+                  "Satisfaction lies in the effort, not in the attainment.\"  \n\n –Mahatma Gandhi","Keep your head in the game.\" \n\n–unknown",
+                  "The price of success is hard work, dedication to the job at hand, and the determination that whether we win or lose, we have applied the best of ourselves to the task at hand.\" –Vince Lombardi",
+                  "If you try and lose then it isn’t your fault. But if you don’t try and lose, then it’s all your fault.\" -Orson Scott Card",
+                  "Hard work does pay off. \"-Michael Phelps",
+                  "We think, mistakenly, that success is the result of the amount of time we put in at work, instead of the quality of time we put in.\"-Ariana Huffington",
+                  "There is no development physically or intellectually without effort, and effort means work.\"-Calvin Coolidge",
+                  "Successful people are not gifted; they just work hard, then succeed on purpose. \"-G.K. Neilson"
+]
+    func pickQuote() -> String {
+        let random = Int.random(in:0..<5)
+        let quote = quotes[random]
+      return quote
+    }
     var body: some View {
         ZStack {
             Image("meadow")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .ignoresSafeArea()
-            Text("\"Success isn’t always about greatness. It’s about consistency. Consistent hard work leads to success. Greatness will come.\" \n\n -Dwayne Johnson")
+            let randomQuote = pickQuote()
+                  Text("\"\(randomQuote)")
                 .font(.title2)
                 .frame(width: 225)
                 .lineLimit(nil)
